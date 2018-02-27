3c35a41767e9234e3ff25b4e836d70f67aa4dbb5
Hanif Fakira <fakira.work@gmail.com>
2018-02-27 15:03:41 +0000

    styling via css, parent specific

--- src/main/java/MyFxApp.java
+++ src/main/java/MyFxApp.java
@@ -1,18 +1,24 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MyFxApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("My First JavaFX App");
-        // Setting scene
-        Label label = new Label("Hello World, JavaFX !");
-        Scene scene = new Scene(label, 400, 200);
-        // Setting styling via css
-        scene.getStylesheets().add("button-styles.css");
-        primaryStage.setScene(scene);
+
+        Button button1 = new Button("Button 1");
+        Button button2 = new Button("Button 2");
+        button1.getStyleClass().add("button1");
+
+        VBox vbox = new VBox(button1, button2);
+        vbox.getStyleClass().add("vbox");
+
+        Scene scene1 = new Scene(vbox, 400, 200);
+        scene1.getStylesheets().add("button-styles.css");
+        primaryStage.setScene(scene1);
 
         primaryStage.show();
     }
